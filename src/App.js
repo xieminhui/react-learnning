b09667df82c8d3a1624e1727dda2c1bf0056b7e4
xieminhui <xmh19940228@163.com>
2021-04-23 16:59:27 +0800

    update react-redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 /*
  * @Date: 2021-04-22 11:00:33
  * @LastEditors: xieminhui
- * @LastEditTime: 2021-04-23 16:33:21
+ * @LastEditTime: 2021-04-23 16:58:58
  * @description: 
  */
 // import logo from './logo.svg';
 import './App.css';
 import Routers from './app/router'
-import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './app/reducers'
 const store = createStore(rootReducer);
