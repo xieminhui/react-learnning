0d692a2b8781c1f1346979211a0fcd8c5aebb3f5
xie <xieminhui228@gmail.com>
2021-04-25 17:01:06 +0800

    update react hook

--- src/app/pages/swiper/index.jsx
+++ src/app/pages/swiper/index.jsx
@@ -1,6 +1,6 @@
 
-import { useState } from 'react';
-import {shuffle} from '../../utils'
+import { useState, useEffect} from 'react';
+// import {shuffle} from '../../utils'
 
 import Item from './item'
 import Button from '../../component/Button'
@@ -21,26 +21,63 @@ const Swiper = () => {
     ];
     const [list, changeData] = useState([
         {
-            imgUrl: 'https://th.bing.com/th/id/R61c7193acb2da97b8e3bf263a679000e?rik=MEDPVg6mouoAGg&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131226%2f1-131226222342.jpg&ehk=MX8xKkMzrBhPZnUNCDf9w0A1AbUr2vqut9ZklZBukMs%3d&risl=&pid=ImgRaw'
+            imgUrl: 'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/baike/pic/item/1e30e924b899a901aa55411415950a7b0208f5b4.jpg',
+            id: 1
         },
         {
-            imgUrl: 'https://th.bing.com/th/id/R788a3ca3fc34a8f457b6a9a582cbebd6?rik=Nlwza%2bi%2f1I2W4Q&riu=http%3a%2f%2fimg.pptjia.com%2fimage%2f20180117%2f16e6d1b2adcb7796e411899957f5f686.jpg&ehk=4%2bqM6f6DYFdLm6Cu4IPioBdsQT5fiqSYmQ073igic8Y%3d&risl=&pid=ImgRaw'
+            imgUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2006287671,2688397669&fm=26&gp=0.jpg',
+            id: 2
         },
         {
-            imgUrl: 'https://th.bing.com/th/id/Rc3d7a0980556453c518fc54e8c31d376?rik=ePW3dX%2b1EZOTDQ&riu=http%3a%2f%2fpicture.ik123.com%2fuploads%2fallimg%2f190126%2f4-1Z126160118.jpg&ehk=lhYfiFZaHVUdi8YdB0mZ2QX5pnQhtOsiGY%2brRiDMunc%3d&risl=&pid=ImgRaw'
+            imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3331665880,2001605345&fm=26&gp=0.jpg',
+            id: 3
         }
     ]);
+    const [init, changeInit] = useState(false)
+    const [currentPage, changCurrentPage] = useState(1)
+    useEffect(() => {
+        const ret = deleteFirst(list);
+        const data = pushData(ret);
+        changeData(data);
+    }, [currentPage])
+    const getRenderStyle = (index) => {
+        if(!init) return;
+        if(currentPage === index + 1 ) {
+            return {
+                transform: 'translate(200px, 0px) rotate(30deg)',
+                opacity: 0,
+                transition: 'all 0.6s'
+            }
+        } else {
+            return {
+                background: 'red'
+            }
+            
+        }
+    }
 
     const change = () => {
-        const arr = shuffle(data);
-        changeData(arr);
+        changCurrentPage(currentPage+1);
+        changeInit(true);
+    }
+
+    const deleteFirst= (arr) => {
+         arr.shift();
+         return arr;
+    }
+
+    const pushData = (arr) => {
+        let newItem = data[Math.random() > 0.5 ? 0 : 1];
+        newItem.id = list[list.length-1].id + 1;
+        arr.push(newItem);
+        return arr;
     }
 
     return (
         <div className={styles['swiper-container']}>
             {
                 list.map((v, i) => 
-                    <Item key = { i } {...v} i={i}></Item>
+                    <Item key = { v.id } {...v} i = {i} style = {getRenderStyle(v.id)}></Item>
                 )
             }
             <Button text="下一个" handleClick={() => change()}></Button>
--- src/app/pages/swiper/item.jsx
+++ src/app/pages/swiper/item.jsx
@@ -1,8 +1,9 @@
 import styles from './index.module.less'
 
 const Item = (props) => (
-    <section className={`${styles.swiper} ${styles[`swiper${props.i}`]}`}>
+    <section className={`${styles.swiper} ${styles[`swiper${props.i}`]}`} style={props.style}>
         <img className={`${styles.img}`} src= {props.imgUrl} alt="加载失败" />
+        <p className={`${styles.des}`}>第{props.id}张</p>
     </section>
 )
 
