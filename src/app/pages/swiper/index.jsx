8bdcbe20ddc0b80fd8c01bfb2d3eeb8f0df76b09
xie <xieminhui228@gmail.com>
2021-04-26 21:59:39 +0800

    add react useEffect hook

--- src/app/pages/swiper/index.jsx
+++ src/app/pages/swiper/index.jsx
@@ -1,5 +1,5 @@
 
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 // import {shuffle} from '../../utils'
 
 import Item from './item'
@@ -9,40 +9,45 @@ import styles from './index.module.less'
 
 let cycleId = 3;
 
+const data = [
+    {
+        imgUrl: 'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/baike/pic/item/1e30e924b899a901aa55411415950a7b0208f5b4.jpg'
+    },
+    {
+        imgUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2006287671,2688397669&fm=26&gp=0.jpg'
+    },
+    {
+        imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3331665880,2001605345&fm=26&gp=0.jpg'
+    }
+];
+
+function fetchMock () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(data);
+        }, 100);
+    })
+}
+
 const Swiper = () => {
-    const data = [
-        {
-            imgUrl: 'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/baike/pic/item/1e30e924b899a901aa55411415950a7b0208f5b4.jpg'
-        },
-        {
-            imgUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2006287671,2688397669&fm=26&gp=0.jpg'
-        },
-        {
-            imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3331665880,2001605345&fm=26&gp=0.jpg'
-        }
-    ];
-    const [list, changeData] = useState([
-        {
-            imgUrl: 'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/baike/pic/item/1e30e924b899a901aa55411415950a7b0208f5b4.jpg',
-            id: 1
-        },
-        {
-            imgUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2006287671,2688397669&fm=26&gp=0.jpg',
-            id: 2
-        },
-        {
-            imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3331665880,2001605345&fm=26&gp=0.jpg',
-            id: 3
-        }
-    ]);
     const [init, changeInit] = useState(false)
     const [animationEnd, changeAnimation] = useState(false)
     const [currentPage, changCurrentPage] = useState(1)
-    // useEffect(() => {
-    //     const ret = deleteFirst(list);
-    //     const data = pushData(ret);
-    //     changeData(data);
-    // }, [currentPage])
+    const [list, changeData] = useState([]);
+    useEffect(() => {
+        // 模拟http请求
+        async function fetchData() {
+            // You can await here
+            const ret = await fetchMock();
+            const initList = ret.map((v ,i)=> {
+                let obj = Object.assign({}, v);
+                obj.id = i + 1;
+                return obj;
+            })
+            changeData(initList);
+          }
+          fetchData();
+    }, [])
     const getRenderStyle = (index) => {
         if(!init) return;
         if(currentPage === index + 1 ) {
